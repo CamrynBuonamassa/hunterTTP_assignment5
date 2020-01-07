b82ef666591cabde98c4828291a228df7c305fe9
Camryn Buonamassa <camrynbuonamassa@gmail.com>
2020-01-06 20:15:16 -0500

    added addCol

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let numRows = 1;
 let numCols = 1;
+let newColor = "pink";
 
 var addRow = function(){
 	var table = document.getElementById("table");
@@ -12,3 +13,19 @@ var addRow = function(){
 	numRows++;
 }
 document.getElementById("button1").addEventListener("click", addRow);
+
+var addCol = function(){
+	var table = document.getElementById("table");
+	for(let i = 0; i < numRows; i++){
+		var row = table.rows[i];
+		var cell = document.createElement("td");
+		row.appendChild(cell);
+	}
+	numCols++;
+}
+document.getElementById("button2").addEventListener("click", addCol);
+
+
+
+
+
